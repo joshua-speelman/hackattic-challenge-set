47ea33a32be6702b421dc80fdbe4f52caed6950c
Joshua Speelman <joshua.m.speelman@gmail.com>
2025-03-01 15:57:42 +1100

    adding mini-miner index.js

--- mini-miner/index.js
+++ mini-miner/index.js
@@ -1,7 +1,14 @@
 const axios = require("axios");
 
 const main = async () => {
+    getProblemSet();
 
+}
+
+const getProblemSet = async () => {
+    const response = await axios.get("https://hackattic.com/challenges/mini_miner/problem?access_token=bfcbcc18d9de2c55");
+    console.log(response.data);
+    return response.data;
 }
 
 main();
