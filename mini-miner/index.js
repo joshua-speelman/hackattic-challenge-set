80bed684035665d5c057aa3e47e931282dffccdd
Joshua Speelman <joshua.m.speelman@gmail.com>
2025-03-08 10:01:30 +1000

    fix: linting

--- mini-miner/index.js
+++ mini-miner/index.js
@@ -1,14 +1,15 @@
 const axios = require("axios");
 
 const main = async () => {
-    getProblemSet();
-
-}
+  getProblemSet();
+};
 
 const getProblemSet = async () => {
-    const response = await axios.get("https://hackattic.com/challenges/mini_miner/problem?access_token=bfcbcc18d9de2c55");
-    console.log(response.data);
-    return response.data;
-}
+  const response = await axios.get(
+    "https://hackattic.com/challenges/mini_miner/problem?access_token=bfcbcc18d9de2c55"
+  );
+  console.log(response.data);
+  return response.data;
+};
 
 main();
